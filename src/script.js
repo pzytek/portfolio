b52e6276b6092b6433ce2b6530015877986cf1c4
Piotr Zytek <piotr.zytek@gmail.com>
2023-09-27 12:31:26 +0200

    added contact form

--- src/script.js
+++ src/script.js
@@ -4,6 +4,12 @@ const expSection = document.querySelector(".sec-exp");
 const projSection = document.querySelector(".sec-proj");
 const contSection = document.querySelector(".sec-cont");
 
+const contactForm = document.querySelector("#contact-form");
+const namee = document.querySelector("#name");
+const email = document.querySelector("#email");
+const subject = document.querySelector("#subject");
+const text = document.querySelector("#text");
+
 function changeActive(itemIndex) {
   listItems.forEach((item, index) => {
     const isCurrentItem = index === itemIndex;
@@ -34,4 +40,26 @@ listItems.forEach((item) => {
   item.addEventListener("mouseleave", () => {
     item.querySelector("span").classList.remove("head-list__span--hovered");
   });
+});
+
+contactForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("data sent");
+  const params = {
+    name: namee.value,
+    email: email.value,
+    subject: subject.value,
+    message: text.value,
+  };
+  emailjs
+    .send("service_tyxryes", "template_hgnxywg", params)
+    .then((res) => {
+      alert(`Success! Your message has been sent. Res.status: ${res.status}`);
+    })
+    .catch((error) => {
+      console.error("Error sending the message:", error);
+      alert(
+        "An error occurred while sending the message. Please try again later."
+      );
+    });
 });
